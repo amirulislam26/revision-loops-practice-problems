bad0309472dce7c1968daee34f852c6b54c2c0b8
amirulislam26 <26techamirulislam@gmail.com>
2022-09-22 23:23:06 -0700

    array and loop js added a commit

---
+++ array-loop.js
@@ -0,0 +1,17 @@
+// for(var i =0; i < 7 ; i++){
+//     console.log(i);
+// };
+
+var numbers = [52, 54, 45, 25, 78, 855, 45, 65, 69, 36, 458];
+for(var i =0; i < numbers.length ; i++){
+    number = numbers [i];
+         console.log(number);
+     };
+
+
+     var items = ['amirul', 'monirul', "khairul", 'imo'];
+     
+     for(var i =0; i < items.length ; i++){
+        item = items [i];
+             console.log(item);
+         };
