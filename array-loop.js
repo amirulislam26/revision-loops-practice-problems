3ae8f19f813c3b8edcd3ca2204fc830fa5686b38
amirulislam26 <26techamirulislam@gmail.com>
2022-09-22 23:37:45 -0700

    added a break js

--- array-loop.js
+++ array-loop.js
@@ -3,10 +3,10 @@
 // };
 
 var numbers = [52, 54, 45, 25, 78, 855, 45, 65, 69, 36, 458];
-for(var i =0; i < numbers.length ; i++){
-    number = numbers [i];
-         console.log(number);
-     };
+// for(var i =0; i < numbers.length ; i++){
+//     number = numbers [i];
+//          console.log(number);
+//      };
 
 
      var items = ['amirul', 'monirul', "khairul", 'imo'];
---
+++ break.js
@@ -0,0 +1,18 @@
+// var numbers = [52, 54, 45, 25, 78, 855, 45, 65, 69, 36, 458];
+//  for(var i =0; i < numbers.length ; i++){
+//     number = numbers [i];
+//     if(number > 100){
+//         break
+//     }
+//          console.log(number);
+//      };
+
+ var items = ['amirul', 'monirul', "khairul", 'imo'];
+     
+ for(var i =0; i < items.length ; i++){
+    item = items [i];
+    if(item == 'khairul'){
+        break
+    }
+         console.log(item);
+     };
