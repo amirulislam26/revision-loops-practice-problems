b5da1937e7a2d715088e7521f281c8f77efeebd1
amirulislam26 <26techamirulislam@gmail.com>
2022-09-22 22:32:55 -0700

    added a while loop and dibagin output

---
+++ while.js
@@ -0,0 +1,12 @@
+// if (condition){
+
+// }
+
+var roastGiven = 0;
+while (roastGiven < 7) {
+    console.log('Roast Den , Pleace !!')
+    console.log(roastGiven);
+    // roastGiven = roastGiven + 1;
+    // roastGiven += 1;
+    roastGiven++;
+}
