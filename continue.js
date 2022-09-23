9dc78b858c9a3207333daa009aca58f4bf204899
amirulislam26 <26techamirulislam@gmail.com>
2022-09-22 23:44:33 -0700

    my js added a continue dibaging

---
+++ continue.js
@@ -0,0 +1,8 @@
+var numbers = [52, 54, 45, 25, 78, 855, 45, 65, 69, 36, 458];
+ for(var i =0; i < numbers.length ; i++){
+     number = numbers [i];
+     if(number > 50){
+        continue;
+     }
+         console.log(number);
+     };
