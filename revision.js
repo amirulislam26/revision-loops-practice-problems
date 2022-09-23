b92a2bdd7fff4c4601358f5dc65a6864cf6dc211
amirulislam26 <26techamirulislam@gmail.com>
2022-09-22 22:15:18 -0700

    my revision loops practice problems-solved

---
+++ revision.js
@@ -0,0 +1,28 @@
+// String type variable
+var bottle = 'water';
+var bootleColor = 'yellow';
+// number type variable
+var sunglassPrice = 120;
+var penColor = 'blue';
+// boolen type variable
+var isExpensive = true;
+
+var tableItems = ['bottle', 'pen', 'book', "paper"];
+
+var item3 = tableItems[3];
+tableItems[4] = 'noteBook';
+var penIndex = tableItems.indexOf('pen');
+//push pop
+
+
+//conditionals
+
+if (tableItems.length > 4){
+
+}
+else if (tableItems[3] == 'pen'){
+
+}
+else{
+    
+}
