c84acc9fa08c060b9e90ab4eec8f94d3406a20bf
amirulislam26 <26techamirulislam@gmail.com>
2022-09-22 23:06:54 -0700

    javascript is my best output for loopadded a commit

---
+++ for.js
@@ -0,0 +1,29 @@
+// var roastGiven = 0;
+// while(roastGiven < 7 ) {
+//     console.log('Plz hire me!!')
+//     roastGiven++;
+// };
+
+
+// for loop......................
+
+// for(var roastGiven =0; roastGiven < 7 ; roastGiven++){
+//     console.log('plz hire me!!')
+// };
+
+//simple for loop......................
+
+// console.log('simple for loop!!')
+// for(var i =0; i < 7 ; i++){
+//     console.log(i);
+// };
+//.............................
+
+// for(var i =0; i <= 20 ; i += 2){
+//     console.log(i);
+// };
+//................................
+
+for(var i =1; i <= 20 ; i += 2){
+    console.log(i);
+};
