d363b72ed2c8b4dfb15de5ad9da3bef7729dbfff
amirulislam26 <26techamirulislam@gmail.com>
2022-09-23 21:59:57 -0700

    added a apple-orange

---
+++ apple-orange.js
@@ -0,0 +1,8 @@
+var monyGiven = 1000;
+var applePrice = 400;
+var orangePrice = 300;
+
+var totalPrice = applePrice + orangePrice;
+console.log(totalPrice);
+var getBack = monyGiven - totalPrice;
+console.log(getBack);
